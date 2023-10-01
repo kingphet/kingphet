00b31564adb92a5808f1ae3b7b63fd9569c5f321
kingphet <133629819+kingphet@users.noreply.github.com>
2023-10-01 21:33:05 +0700

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,13 +3,13 @@ export default function Navbar() {
     <>
       <div className="w-full h-24 bg-violet-900 py-5 text-slate-200">
         <div className="w-full h-full flex items-end justify-end px-20">
-          <a href="" className="text-2xl mr-5">
+          <a href="" className="text-2xl mr-5 hover:underline">
             Home
           </a>
-          <a href="" className="text-2xl mr-5">
+          <a href="" className="text-2xl mr-5 hover:underline">
             Contect
           </a>
-          <a href="" className="text-2xl mr-5">
+          <a href="" className="text-2xl mr-5 hover:underline">
             About
           </a>
         </div>
